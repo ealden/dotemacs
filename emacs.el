00ad7bb79794a607cd6fbafa8314f4f3450547c8
Ealden Esto E. Escanan <ealden@gmail.com>
2012-05-26 21:49:11 +0800

    Initial emacs.el file
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

---
+++ emacs.el
@@ -0,0 +1,30 @@
+(set-face-attribute 'default nil :font "Inconsolata-12")
+(ns-toggle-fullscreen)
+(windmove-default-keybindings)
+
+(custom-set-variables
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(nxml-bind-meta-tab-to-complete-flag t)
+ '(nxml-child-indent 2)
+ '(nxml-outline-child-indent 2)
+ '(nxml-slash-auto-complete-flag t)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(global-font-lock-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(use-dialog-box nil)
+ '(inhibit-startup-screen t)
+ '(indent-tabs-mode nil))
+(custom-set-faces)
+
+(setq emacs.d-path "~/.emacs.d")
+(setq site-lisp-path (concat emacs.d-path "/site-lisp"))
+(setq etc-path (concat emacs.d-path "/etc"))
+
+(add-to-list 'load-path site-lisp-path)
+
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
