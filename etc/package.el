3aa41f247e414c25952a9d4b5027cc98ed85ca84
Ealden Esto E. Escanan <ealden@gmail.com>
2012-12-15 10:05:16 +0800

    Reenabled Marmalade for PeepOpen
    PeepOpen doesn't seem to be in MELPA.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- etc/package.el
+++ etc/package.el
@@ -3,8 +3,8 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;;(add-to-list 'package-archives
-;;             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
